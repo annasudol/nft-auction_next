2338b19fcddd394d24953c30961331cbeeb74d75
Anna Sudol <sudolann@gmail.com>
2023-09-09 16:31:37 +0200

    chore: added navigation

---
+++ pages/account.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import Layout from '@/components/layout';
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <p>account</p>
+    </Layout>
+  );
+}
