2980d1451bbbbfc3786df982bd21d6b954baba5f
Anna Sudol <sudolann@gmail.com>
2023-09-10 16:04:52 +0200

    fix: counterInput validation 2

--- src/components/Input/CounterInput.tsx
+++ src/components/Input/CounterInput.tsx
@@ -1,41 +1,32 @@
-import clsx from 'clsx';
 import { FC, InputHTMLAttributes } from 'react';
-
-import s from './Input.module.css';
 
 import Button from '@/components/Button';
 
 export interface InputMinMaxProps
   extends InputHTMLAttributes<HTMLInputElement> {
   handleChange: (value: number) => void;
-  register: any,
-  name: string;
-  isValid?: boolean;
+  value: number;
 }
 
 const CounterInput: FC<InputMinMaxProps> = ({
   handleChange,
-  isValid = true,
-  register,
-  name,
+  value,
   ...props
 }) => {
-  console.log(props, 'props');
   return (
     <div className='my-2 flex w-36 items-center'>
       <Button
         additionalClasses='rounded-r-none h-10'
         variant='light'
-        onClick={() => handleChange(-1)}
+        onClick={() => handleChange(1)}
       >
         -
       </Button>
       <input
-        className={clsx(s.input, 'h-10 border-x-0 px-4', {
-          '': isValid,
-          '': isValid,
-        })}
-        { ...register(name)}
+        type='text'
+        pattern='[0-9]*'
+        value={value}
+        {...props}
       />
       <Button
         additionalClasses='rounded-l-none h-10'
